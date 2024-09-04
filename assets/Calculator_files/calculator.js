c10320975ee0c6577130ca279b95fabe8ab3daa1
Milan Chaudhary <Chaudharymilan419@gmail.com>
2024-09-04 19:36:28 +1000

    first commit

---
+++ assets/Calculator_files/calculator.js
@@ -0,0 +1,20 @@
+let colorElement = document.getElementsByClassName("yellow");
+for (i = 0; i < colorElement.length; i++) {
+  colorElement[i].style.backgroundColor = "#ff9500";
+  colorElement[i].style.borderRadius = "50% ";
+  colorElement[i].style.border = "0px ";
+}
+
+let colorElement1 = document.getElementsByClassName("lightgrey");
+for (i = 0; i < colorElement1.length; i++) {
+  colorElement1[i].style.backgroundColor = " #d4d4d2";
+  colorElement1[i].style.borderRadius = "50% ";
+  colorElement1[i].style.border = "0px ";
+}
+
+let colorElement2 = document.getElementsByClassName("darkgrey");
+for (i = 0; i < colorElement2.length; i++) {
+  colorElement2[i].style.backgroundColor = " #505050";
+  colorElement2[i].style.borderRadius = "50% ";
+  colorElement2[i].style.border = "0px ";
+}
